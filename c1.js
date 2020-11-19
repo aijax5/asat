f3cf306f433e2da0aeaa5cbb60ff34dae0bf1eba
aijax5 <mdajaaz1999@gmail.com>
2020-11-19 21:06:01 +0530

    merge conflicts resolved

--- c1.js
+++ c1.js
@@ -1,43 +1,3 @@
-<<<<<<< HEAD
-const io = require("socket.io-client");
-const rl = require('console-read-write');
-
-async function main(){
-
-    // const rl = readline.createInterface({
-    //   input: process.stdin,
-    //   output: process.stdout
-    // });
-
-
-    ioClient = io.connect("http://localhost:3000");
-
-    //catching events from server
-    ioClient.on("hello-client", (data)=>{
-        console.log("server said "+data);
-
-    });
-
-    while(true){
-        console.log("\n\n\n");
-        // _ = await io.ask('enter search string')
-        search_str = await io.ask('enter search string')
-        // awaitrl.question("enter search string", (search_str) => {
-        //     ioClient.emit('search', search_str);      
-        //     rl.close();
-        //   });
-
-        ioClient.emit('search', search_str);
-        ioClient.on('search_res', (response)=>{
-            console.log("query response from the server was"+response);
-        });
-        // console
-    }
-};
-
-
-main();
-=======
 const io_c = require("socket.io-client");
 var app = require('express')();
 var http = require('http').createServer(app);
@@ -76,4 +36,3 @@ ioClient.on("hello-client", (data)=>{
 
 // clearTimeout(myVar);
 // ioClient.emit("hello-server","aisi sexy lagraha tha!");
->>>>>>> b907c6e5dcf8cde48cf7912bc1d744f38add9cc2
--- c2.js
+++ c2.js
@@ -1,37 +1,3 @@
-<<<<<<< HEAD
-const io = require("socket.io-client");
-const readline = require('readline');
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-
-ioClient = io.connect("http://localhost:3000");
-
-//catching events from server
-ioClient.on("hello-client", (data)=>{
-    console.log("server said "+data);
-
-});
-
-// while(true){
-    console.log("\n\ninput any key to start search");
-
-    // rl.question("enter search string", (search_str) => {
-    //     ioClient.emit('search', search_str);      
-    //     rl.close();
-    //   });
-
-    ioClient.emit('search', "sashi_latest_albums.mp3");
-    ioClient.on('search_res', (response)=>{
-        console.log("query response from the server was"+response);
-    });
-    // console
-// }
-   
-=======
 const io_c = require("socket.io-client");
 var app = require('express')();
 var http = require('http').createServer(app);
@@ -50,4 +16,3 @@ ioClient.on("hello-client", (data)=>{
 });
 
 // ioClient.emit("hello-server","aisi sexy lagraha tha!");
->>>>>>> b907c6e5dcf8cde48cf7912bc1d744f38add9cc2
