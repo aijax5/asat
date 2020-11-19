960a57f02e2f01c0e225c7cc62d27e624fa1ae4a
aijax5 <mdajaaz1999@gmail.com>
2020-11-19 20:24:59 +0530

    gc

--- c1.js
+++ c1.js
@@ -1,11 +1,38 @@
 const io = require("socket.io-client");
+const rl = require('console-read-write');
 
-ioClient = io.connect("http://localhost:3000");
+async function main(){
 
-//catching events from server
-ioClient.on("hello-client", (data)=>{
-    console.log("server said "+data);
-    ioClient.emit("hello-server","aisi sexy lagraha tha!");
-});
+    // const rl = readline.createInterface({
+    //   input: process.stdin,
+    //   output: process.stdout
+    // });
 
-// ioClient.emit("hello-server","aisi sexy lagraha tha!");
+
+    ioClient = io.connect("http://localhost:3000");
+
+    //catching events from server
+    ioClient.on("hello-client", (data)=>{
+        console.log("server said "+data);
+
+    });
+
+    while(true){
+        console.log("\n\n\n");
+        // _ = await io.ask('enter search string')
+        search_str = await io.ask('enter search string')
+        // awaitrl.question("enter search string", (search_str) => {
+        //     ioClient.emit('search', search_str);      
+        //     rl.close();
+        //   });
+
+        ioClient.emit('search', search_str);
+        ioClient.on('search_res', (response)=>{
+            console.log("query response from the server was"+response);
+        });
+        // console
+    }
+};
+
+
+main();
---
+++ c2.js
@@ -0,0 +1,32 @@
+const io = require("socket.io-client");
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+
+ioClient = io.connect("http://localhost:3000");
+
+//catching events from server
+ioClient.on("hello-client", (data)=>{
+    console.log("server said "+data);
+
+});
+
+// while(true){
+    console.log("\n\ninput any key to start search");
+
+    // rl.question("enter search string", (search_str) => {
+    //     ioClient.emit('search', search_str);      
+    //     rl.close();
+    //   });
+
+    ioClient.emit('search', "sashi_latest_albums.mp3");
+    ioClient.on('search_res', (response)=>{
+        console.log("query response from the server was"+response);
+    });
+    // console
+// }
+   
